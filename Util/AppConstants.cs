6bd8b4a48e5d36399236a42605a98eff4777fdec
Up <upcraftedlp@gmail.com>
2023-08-24 12:07:37 +0200

    add constants class

---
+++ Util/AppConstants.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace Up.Bsky.PostBot.Util;
+
+public static class AppConstants
+{
+    public static readonly string AppName = Assembly.GetEntryAssembly()?.GetName().FullName ?? "BlueSky Post Bot";
+}
