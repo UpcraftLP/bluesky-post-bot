c87f182a26455d312a58de3fb0bc03673a6e20e4
Up <upcraftedlp@gmail.com>
2023-08-24 14:42:23 +0200

    clean up DB on guild leave

--- Services/Discord/JoinMessageService.cs
+++ Services/Discord/JoinMessageService.cs
@@ -1,6 +1,8 @@
 ﻿using Discord.Addons.Hosting;
 using Discord.Interactions;
 using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
+using Up.Bsky.PostBot.Database;
 
 namespace Up.Bsky.PostBot.Services.Discord;
 
@@ -36,8 +38,20 @@ public class JoinMessageService : DiscordClientService
             Logger.LogInformation("Bot left guild ({GuildId}): {GuildName}", guild.Id, guild.Name);
             var updateStatusService = scope.ServiceProvider.GetRequiredService<IUpdateStatusService>();
             await updateStatusService.UpdateStatus(stoppingToken);
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
             
-            //TODO remove all channels in guild from DB, remove all unused users from DB, remove all orphan posts from DB
+            var trackedChannels = await dbContext.DiscordChannels.Where(it => it.ServerId == guild.Id).ToListAsync(stoppingToken);
+            dbContext.DiscordChannels.RemoveRange(trackedChannels);
+
+            var users = await dbContext.TrackedUsers.Where(it => it.TrackedInChannels.Count == 0).Include(bskyUser => bskyUser.Posts).ToListAsync(stoppingToken);
+            var posts = users.SelectMany(it => it.Posts).ToList();
+            dbContext.RemoveRange(posts);
+            dbContext.TrackedUsers.RemoveRange(users);
+            
+            Logger.LogInformation("Removing {UserCount} users with {PostCount} posts from database", users.Count, posts.Count);
+            
+            await dbContext.SaveChangesAsync(stoppingToken);
         };
         await Task.Delay(-1, stoppingToken);
     }
