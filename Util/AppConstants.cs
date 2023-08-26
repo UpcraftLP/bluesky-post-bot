38de7bc10eae25bf97554827180dc0e424c40066
Up <upcraftedlp@gmail.com>
2023-08-26 11:50:21 +0200

    move permissions to app constants

--- Util/AppConstants.cs
+++ Util/AppConstants.cs
@@ -1,8 +1,23 @@
 ﻿using System.Reflection;
+using Discord;
 
 namespace Up.Bsky.PostBot.Util;
 
 public static class AppConstants
 {
     public static readonly string AppName = Assembly.GetEntryAssembly()?.GetName().Name ?? "BlueSky Post Bot";
+
+    public static readonly ApplicationInstallParams BotPermissions = new(new[]
+        {
+            "bot",
+            "applications.commands",
+        },
+        GuildPermission.ManageWebhooks
+        | GuildPermission.SendMessages
+        | GuildPermission.EmbedLinks
+        | GuildPermission.AttachFiles
+        | GuildPermission.MentionEveryone
+        | GuildPermission.UseExternalEmojis
+        | GuildPermission.AddReactions
+    );
 }
--- Util/Discord/Commands/InviteCommand.cs
+++ Util/Discord/Commands/InviteCommand.cs
@@ -1,5 +1,4 @@
-﻿using Discord;
-using Discord.Interactions;
+﻿using Discord.Interactions;
 
 namespace Up.Bsky.PostBot.Util.Discord.Commands;
 
@@ -8,21 +7,6 @@ public class InviteCommand : InteractionModuleBase<SocketInteractionContext>
     [SlashCommand("invite", "Generate an invite link for this bot")]
     public async Task GenerateInviteLink()
     {
-        var requiredPermissions = new ApplicationInstallParams(
-            new[]
-            {
-                "bot",
-                "applications.commands",
-            },
-            GuildPermission.ManageWebhooks
-            | GuildPermission.SendMessages
-            | GuildPermission.EmbedLinks
-            | GuildPermission.AttachFiles
-            | GuildPermission.MentionEveryone
-            | GuildPermission.UseExternalEmojis
-            | GuildPermission.AddReactions
-        );
-
-        await RespondAsync($"https://discord.com/api/oauth2/authorize?client_id={Context.Client.CurrentUser.Id}&permissions={(ulong) requiredPermissions.Permission}&scope={string.Join("%20", requiredPermissions.Scopes)}", ephemeral: true);
+        await RespondAsync($"https://discord.com/api/oauth2/authorize?client_id={Context.Client.CurrentUser.Id}&permissions={(ulong) AppConstants.BotPermissions.Permission}&scope={string.Join("%20", AppConstants.BotPermissions.Scopes)}", ephemeral: true);
     }
 }
